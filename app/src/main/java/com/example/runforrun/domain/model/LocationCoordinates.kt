a1b8d2cbcb656039fff1c55d93fc85dd1304eff2
M-u-qp <muqpu4eckui@gmail.com>
2024-09-22 15:40:37 +0300

    Delete and refactoring

--- app/src/main/java/com/example/runforrun/domain/model/LocationCoordinates.kt
+++
@@ -1,6 +0,0 @@
-package com.example.runforrun.domain.model
-
-data class LocationCoordinates(
-    val latitude: Double,
-    val longitude: Double
-)
--- app/src/main/java/com/example/runforrun/domain/model/LocationTracking.kt
+++
@@ -1,6 +0,0 @@
-package com.example.runforrun.domain.model
-
-data class LocationTracking(
-    val locationCoordinates: LocationCoordinates,
-    val speed: Float
-)
