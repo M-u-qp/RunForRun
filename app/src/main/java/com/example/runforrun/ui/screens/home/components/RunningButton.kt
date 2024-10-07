d15b7e8e4fc831c25d15a4277a709010ca432928
M-u-qp <muqpu4eckui@gmail.com>
2024-10-07 17:34:09 +0300

    Redesign RunningButton

--- app/src/main/java/com/example/runforrun/ui/screens/home/components/RunningButton.kt
+++ app/src/main/java/com/example/runforrun/ui/screens/home/components/RunningButton.kt
@@ -1,15 +1,18 @@
 package com.example.runforrun.ui.screens.home.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
@@ -21,23 +24,48 @@ fun RunningButton(
     modifier: Modifier = Modifier,
     navigateToRun: () -> Unit
 ) {
-    ElevatedButton(
+    Box(
         modifier = modifier
-            .zIndex(1f)
             .size(70.dp)
-            .offset(y = 35.dp),
-        shape = CircleShape,
-        colors = ButtonDefaults.elevatedButtonColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary
-        ),
-        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 8.dp),
-        onClick = navigateToRun
+            .zIndex(1f)
+            .offset(y = 35.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = CircleShape
+            )
+            .shadow(
+                shape = CircleShape,
+                elevation = 8.dp
+            )
+            .clickable { navigateToRun() }
     ) {
-        Icon(
-            modifier = Modifier.scale(2f),
-            bitmap = ImageBitmap.imageResource(id = R.drawable.run_next),
-            contentDescription = null
-        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .padding(5.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    shape = CircleShape
+                )
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(50.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape
+                    )
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(40.dp),
+                    bitmap = ImageBitmap.imageResource(id = R.drawable.run_next),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
     }
 }
