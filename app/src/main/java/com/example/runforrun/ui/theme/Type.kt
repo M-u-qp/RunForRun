e7b419a3d46806d6b7d5e24acc816ab060b95393
M-u-qp <muqpu4eckui@gmail.com>
2024-10-27 17:07:43 +0300

    Fix Type in Theme

--- app/src/main/java/com/example/runforrun/ui/theme/Type.kt
+++ app/src/main/java/com/example/runforrun/ui/theme/Type.kt
@@ -10,6 +10,7 @@ import com.example.runforrun.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    //BODY
     bodyLarge = TextStyle(
         fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
         fontWeight = FontWeight.Normal,
@@ -17,6 +18,21 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    //TITLE
     titleLarge = TextStyle(
         fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
         fontWeight = FontWeight.Normal,
@@ -24,6 +40,21 @@ val Typography = Typography(
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.2.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    //LABEL
     labelSmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
         fontWeight = FontWeight.Medium,
@@ -31,6 +62,21 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    //DISPLAY
     displaySmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
         fontWeight = FontWeight.Normal,
@@ -38,7 +84,6 @@ val Typography = Typography(
         lineHeight = 44.sp,
         letterSpacing = 0.0.sp
     ),
-
     displayMedium = TextStyle(
         fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
         fontWeight = FontWeight.Normal,
@@ -53,6 +98,26 @@ val Typography = Typography(
         lineHeight = 64.sp,
         letterSpacing = (-0.2).sp
     ),
-
-
+    //HEADLINE
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.gentium_book_plus_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.0.sp
+    )
 )
