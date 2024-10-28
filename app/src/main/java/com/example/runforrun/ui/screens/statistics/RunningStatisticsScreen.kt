f2cb42f246d346ac0037c91adcceeddfe30280f7
M-u-qp <muqpu4eckui@gmail.com>
2024-10-28 10:43:39 +0300

    Delete Statistics

--- app/src/main/java/com/example/runforrun/ui/screens/statistics/RunningStatisticsScreen.kt
+++ app/src/main/java/com/example/runforrun/ui/screens/statistics/RunningStatisticsScreen.kt
@@ -7,29 +7,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.runforrun.R
-import com.example.runforrun.common.extension.toCalendar
-import com.example.runforrun.common.extension.toList
 import com.example.runforrun.ui.components.DefaultTopBar
-import com.example.runforrun.ui.screens.statistics.components.DateRangeCard
-import com.example.runforrun.ui.screens.statistics.components.FilteringStatistic
-import com.example.runforrun.ui.screens.statistics.components.RunningStatisticsGraphCard
 
 @Composable
 fun RunningStatisticsScreen(
     viewModel: RunningStatisticsViewModel = hiltViewModel(), navigateUp: () -> Unit
 ) {
-    val state by viewModel.state.collectAsStateWithLifecycle()
-    val dateList = remember(state.dateRange) {
-        (state.dateRange.start.toCalendar()..state.dateRange.endInclusive.toCalendar()).toList()
-    }
 
     Scaffold(
         modifier = Modifier.fillMaxWidth(),
@@ -49,19 +37,7 @@ fun RunningStatisticsScreen(
                 text = stringResource(id = R.string.weekly_report),
                 style = MaterialTheme.typography.titleMedium
             )
-            DateRangeCard(dateList = dateList.map { it.time },
-                incrementDate = viewModel::incrementRange,
-                decrementDate = viewModel::decrementRange,
-                isDateAvailable = { state.statisticsOnDate.containsKey(it) })
-            FilteringStatistic(
-                selected = state.statistic, selectChip = viewModel::selectStatistic
-            )
-            RunningStatisticsGraphCard(
-                modifier = Modifier.fillMaxWidth(),
-                runningStatistics = state.statisticsOnDate,
-                dateRange = state.dateRange,
-                statistics = state.statistic
-            )
+
         }
     }
 }
--- app/src/main/java/com/example/runforrun/ui/screens/statistics/RunningStatisticsState.kt
+++ app/src/main/java/com/example/runforrun/ui/screens/statistics/RunningStatisticsState.kt
@@ -1,18 +1,12 @@
 package com.example.runforrun.ui.screens.statistics
 
-import com.example.runforrun.common.extension.setFirstDayOfWeek
-import com.example.runforrun.common.extension.setLastDayOfWeek
-import com.example.runforrun.common.extension.setMinimumTime
-import com.example.runforrun.common.extension.toCalendar
 import com.example.runforrun.data.model.Run
-import java.util.Calendar
 import java.util.Date
 
 data class RunningStatisticsState(
     val runList: List<Run>,
     val dateRange: ClosedRange<Date>,
     val statistic: Statistic,
-    val statisticsOnDate: Map<Date, TotalStatisticsOnDate>
 ) {
     enum class Statistic {
         DISTANCE,
@@ -26,41 +20,5 @@ data class RunningStatisticsState(
                 CALORIES -> "Сожженные калории"
             }
         }
-    }
-
-    companion object {
-        val EMPTY
-            get() = RunningStatisticsState(
-                runList = emptyList(),
-                dateRange = Calendar.getInstance().setFirstDayOfWeek().time..
-                        Calendar.getInstance().setLastDayOfWeek().time,
-                statistic = Statistic.DISTANCE,
-                statisticsOnDate = emptyMap()
-            )
-    }
-}
-
-data class TotalStatisticsOnDate(
-    val date: Date = Date(),
-    val distance: Int = 0,
-    val duration: Long = 0L,
-    val caloriesBurned: Int = 0
-) {
-    operator fun plus(other: TotalStatisticsOnDate?) = other?.let {
-        TotalStatisticsOnDate(
-            date = this.date,
-            distance = this.distance + other.distance,
-            duration = this.duration + other.duration,
-            caloriesBurned = this.caloriesBurned + other.caloriesBurned
-        )
-    } ?: this
-
-    companion object {
-        fun running(run: Run) = TotalStatisticsOnDate(
-            date = run.timestamp.toCalendar().setMinimumTime().time,
-            distance = run.distance,
-            duration = run.duration,
-            caloriesBurned = run.caloriesBurned
-        )
     }
 }
